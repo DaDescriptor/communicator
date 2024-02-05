59cf52864a4f66f0374c96f75c365ed7ca2a52b7
DaDescriptor <mineconhoogy@gmail.com>
2024-02-05 08:11:02 +0700

    idk what changed

--- Dconsole/Console.gd
+++ Dconsole/Console.gd
@@ -12,27 +12,52 @@ signal on_send(command: String, arguments: Array, raw_input: String)
 @onready var textTemplate = load("res://Dconsole/output.tscn")
 
 func _ready():
-	print("KILL ME PLEASE")
+	# print("KILL ME PLEASE")
 	sendButton.connect('pressed', _send)
+	inputLine.connect('text_submitted', _send)
+
+func _get_args(text: String) -> Array:
+	var result: Array = []
+	var buffer: String = ""
+	var isString: bool = false
+	
+	# go through every letter in the command and split it into multiple arguments
+	for letter in text:
+		if letter == " " and !isString:
+			result.append(buffer)
+			buffer = ""
+		elif letter == "\"" and buffer[buffer.length() - 2]!="\\":
+			# toggle isString if processed character is unescaped "
+			isString = !isString
+		else:
+			buffer = buffer+letter
+	
+	if isString:
+		output("Error parsing command: doublequotes are not closed.")
+		return []
+	
+	return result
 
 func output(text: String):
-	print(text)
-	
 	var textInstance = textTemplate.instantiate() # create a new log message
-	textInstance.reparent(outputContainer) # move it to the VBoxContainer
+	outputContainer.add_child(textInstance)
 	textInstance.text = text # write text passed in
 
 func _send():
 	var input = inputLine.text
-	print(">>> "+input)
 	output(">>> "+input) # log command written by user
 	
+	var args = _get_args(input).duplicate()
+	args.remove_at(0)
 	
-	
-	on_send.emit(input) # emit send signal to note other scripts
+	on_send.emit(
+		_get_args(input)[0], # command
+		args, #arguments
+		input # raw input string
+	) 
 
-func toggle_visibility(is_visible: bool = !visible):
-	visible = is_visible
+func toggle_visibility(must_show: bool = !visible):
+	visible = must_show
 
 func _process(_delta):
 	if Input.is_action_just_pressed("open_console"):
